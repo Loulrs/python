16b4e972fb74a997b71ae5382f83d60c0ace0fb0
Loulrs <louloutelaurens@gmail.com>
2025-11-17 15:31:35 +0100

    ..

--- Exo1.py
+++ Exo1.py
@@ -1,6 +1,4 @@
-# 1. On déclare d'abord les variables pour que Python les connaisse
 maVariable = 2
 MACONSTANTE = 3
 
-# 2. On utilise la fonction print() avec une f-string pour afficher les deux variables
 print(f"La variable est {maVariable} et la constante est {MACONSTANTE}")
